f14256bc009b30f666943ef48770408494bfd541
ajinkya-cell <ajinkyaadharkar@gmail.com>
2026-04-30 12:36:10 +0530

    lesse

--- apps/excalifn/components/endpoints.tsx
+++ apps/excalifn/components/endpoints.tsx
@@ -1,3 +1,3 @@
 const isProd = process.env.NODE_ENV === "production";
-export const http = isProd ? "https://excali-again-http-server.onrender.com" : "http://localhost:3001";
-export const wslink = isProd ? "wss://excali-http-server.onrender.com" : "ws://localhost:8080";
+export const http = isProd ? "https://excali-http.onrender.com" : "http://localhost:3001";
+export const wslink = isProd ? "wss://excali-ws-25w3.onrender.com" : "ws://localhost:8080";
